8bf61f3d4bad6e3d1bd2338e3ed52e075f703519
Eslam1141 <eslamabdallah1419@outlook.com>
2020-03-18 22:34:12 +0000

    changing mistake

--- vpc.tf
+++ vpc.tf
@@ -12,7 +12,7 @@ resource "aws_vpc" "EMI-vpc" {
 resource "aws_subnet" "EMI-subnet-public-1" {
     vpc_id = "${aws_vpc.EMI-vpc.id}"
     cidr_block = "10.0.1.0/24"
-    map_public_ip_on_launch = "true" //it makes this a public subnet
+    map_public_ip_on_launch = "false" //it makes this a public subnet
     availability_zone = "us-east-2a"
     tags {
         Name = "EMI-subnet-public-1"
@@ -21,7 +21,7 @@ resource "aws_subnet" "EMI-subnet-public-1" {
 resource "aws_subnet" "EMI-subnet-public-2" {
     vpc_id = "${aws_vpc.EMI-vpc.id}"
     cidr_block = "10.0.2.0/24"
-    map_public_ip_on_launch = "true" //it makes this a public subnet
+    map_public_ip_on_launch = "false" //it makes this a public subnet
     availability_zone = "us-east-2b"
     tags {
         Name = "EMI-subnet-public-2"
@@ -30,7 +30,7 @@ resource "aws_subnet" "EMI-subnet-public-2" {
 resource "aws_subnet" "EMI-subnet-public-3" {
     vpc_id = "${aws_vpc.EMI-vpc.id}"
     cidr_block = "10.0.3.0/24"
-    map_public_ip_on_launch = "true" //it makes this a public subnet
+    map_public_ip_on_launch = "false" //it makes this a public subnet
     availability_zone = "us-east-2c"
     tags {
         Name = "EMI-subnet-public-3"
